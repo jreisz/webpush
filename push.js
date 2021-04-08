32881f98b5a8eac09415f42a02db9602549eca7b
jreisz <jreisz@brenock.com>
2021-04-08 20:19:45 -0300

    initial

--- push.js
+++ push.js
@@ -7,7 +7,9 @@ let validKeys = {
 }
 push.setVapidDetails('mailto:test@code.co.uk', validKeys.publicKey, validKeys.privateKey)
 let sub = {
-    "endpoint": "https://fcm.googleapis.com/fcm/send/cTiX8wwKRF8:APA91bFqG6qL84WvPRQl710cAyuHPLRTu_TWg-7-MP8GovdoyMgA9iay5cgfE3T5P9qfEkRjdNdnAWBVeXaamnxjvJPfsk85NshQwWUelzHFEF3esnRF-mCeCxiWJrp6E7II3X8oe19W", "expirationTime": null, "keys": {
+    "endpoint": "https://fcm.googleapis.com/fcm/send/cTiX8wwKRF8:APA91bFqG6qL84WvPRQl710cAyuHPLRTu_TWg-7-MP8GovdoyMgA9iay5cgfE3T5P9qfEkRjdNdnAWBVeXaamnxjvJPfsk85NshQwWUelzHFEF3esnRF-mCeCxiWJrp6E7II3X8oe19W",
+    "expirationTime": null,
+    "keys": {
         "p256dh": "BKMBX3m9ENJHp-Vy-THsdbCk7OiKOwBUqDbqgyaC47ncniWQiE9uysuv2K30hRgGS1A9dNv9giQqQrx-rDcjF9k",
         "auth": "z2jf4sNSpWV_oe8MmGiUPQ"
     }
